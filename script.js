ab17a48fc5be4a8dfbe4b1f1e9b09aebe1f78d59
Ugnė Miklovaitė <136790656+mmiklovaitemm@users.noreply.github.com>
2025-03-20 12:20:52 +0200

    Update script.js

--- script.js
+++ script.js
@@ -19,10 +19,28 @@ score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
 
-const scores = [0, 0];
-let currentScore = 0;
-let activePlayer = 0;
-let playing = true;
+let scores, currentScore, activePlayer, playing;
+
+// NEW GAME function
+const init = function () {
+  // Reset all scores to 0
+  scores = [0, 0];
+  currentScore = 0;
+  activePlayer = 0;
+  playing = true;
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  current0El.textContent = 0;
+  current1El.textContent = 0;
+  // Remove winner class(dark background)
+  player0El.classList.remove('player--winner');
+  player1El.classList.remove('player--winner');
+  player0El.classList.add('player--active');
+  player1El.classList.remove('player--active');
+  // Show roll dice image
+  diceEl.classList.remove('hidden');
+};
+init();
 
 // Switch player function
 const switchPlayer = function () {
@@ -81,3 +99,6 @@ btnHold.addEventListener('click', function () {
     }
   }
 });
+
+// New game
+btnNew.addEventListener('click', init);
